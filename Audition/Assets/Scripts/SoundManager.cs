5d9faee8bde1dfbf5b7c978ca34bd9ce1e5ce924
LongNT <longnt2007@gmail.com>
2020-04-09 17:48:26 +0700

    - Added sample SoundManager to play sound.

---
+++ Audition/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+    private bool isMusicPlaying;
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        isMusicPlaying = false;
+    }
+
+    public void PlaySingle(AudioClip clip)
+    {
+    }
+
+    public void PlayMusic()
+    {
+        GameObject music = GameObject.Find("Music");
+        if(music != null && !isMusicPlaying)
+        {
+            Debug.Log("SoundManager::PlayMusic");
+            music.GetComponent<AudioSource>().Play();
+            isMusicPlaying = true;
+        }
+    }
+
+    public void StopMusic()
+    {
+        GameObject music = GameObject.Find("Music");
+        if(music != null && isMusicPlaying)
+        {
+            Debug.Log("SoundManager::StopMusic");
+            music.GetComponent<AudioSource>().Stop();
+            isMusicPlaying = false;
+        }
+    }
+}
